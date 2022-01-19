3d9a0add2573ea30fda52662b002988137aa224b
Michael Huang <michaelyhuang23@gmail.com>
2022-01-19 03:47:04 -0500

    complete

--- mislead.py
+++ mislead.py
@@ -1,14 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
+import altair as alt
+import numpy as np
+import pandas as pd
 
 data = pd.read_csv("Fake Email.csv")
 cc = Counter()
+ndates = []
 for i, row in data.iterrows():
-	txt = data['title']
-	date = data['date']
-	for word in txt.split():
-		cc[word.lower()]+=1
+    txt = row['title']
+    date = row['date']
+    try:
+        ndates.append(date.split(' ')[0]+" "+date.split(', ')[1])
+    except:
+        ndates.append('Jan 2015')
+        continue
+    for word in txt.split():
+        cc[word.lower()]+=1
+
+data['date']=ndates
 
 front = cc.most_common()[:3]
 print(front)
@@ -17,19 +28,36 @@ front = [fr[0] for fr in front]
 counter = {}
 for i, row in data.iterrows():
 	c = Counter()
-	txt= data['title']
-	date = data['date']
+	txt= row['title']
+	date = row['date']
 	for word in txt.split():
 		if word in front:
 			c[word]+=1
-	if date not in counter: counter[date] = Counter()
+	if not date in counter: counter[date] = Counter()
 	counter[date].update(c)
 
-colors = ['red', 'green', 'blue']
-
+src = []
 for date in counter.keys():
 	c = counter[date]
 	for k, v in c.items():
-		idx = front.index(k)
-		plt.scatter()
+		src.append((date, k, v, ''))
 
+df = pd.DataFrame(src, columns=['date','word','count','col'])
+
+
+colors = ['red', 'green', 'blue']
+cols = []
+for i, row in df.iterrows():
+	cols.append(colors[front.index(row['word'])])
+
+df['col'] = cols
+print(df)
+df = df[['date','col','count']]
+    
+
+
+alt.Chart(df).mark_line().encode(
+    x='date',
+    y='count',
+    color='word'
+)
